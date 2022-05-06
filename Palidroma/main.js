89a8682369e68eeedf2e527dc7f6d4bd6ae82fda
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-06 14:43:48 +0200

    aggiunto logica a js

---
+++ Palidroma/main.js
@@ -0,0 +1,8 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// 1 chiedo all utente la parola
+// 2 creo una funzione con target la parola dell'utente
+// 3 prendo la parola e assegno un indice
+// se capovolgendo la parola è identica compare l'avviso "la parola e palindroma"
+// altrimenti compare l'avviso "la parola non e palindroma"
